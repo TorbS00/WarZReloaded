8755196e477d55e9abd1d2ffa93fd41a6b7f9446
BeastyBoo <torbjorn1.skavhaug@gmail.com>
2021-07-10 21:04:57 +0200

    DataCenter

---
+++ common/src/main/java/com/github/beastyboo/foundation/DataCenter.java
@@ -0,0 +1,37 @@
+package com.github.beastyboo.foundation;
+
+import java.util.concurrent.CompletableFuture;
+
+public interface DataCenter {
+
+    @FunctionalInterface
+    interface TransactionRunner {
+
+        void runTransactUsing(Transaction transaction);
+    }
+
+    @FunctionalInterface
+    interface TransactionActor<R> {
+
+        R transactUsing(Transaction transaction);
+    }
+
+    /**
+     *  Runs a transaction without a return.
+     *
+     * @param runner the transaction body
+     * @return a future implementation (CompletableFuture) once the transaction is complete
+     */
+    CompletableFuture<?> runTransaction(TransactionRunner runner);
+
+    /**
+     *  Runs the transaction with a return.
+     *
+     * @param actor the transaction body
+     * @param <R> the result type
+     * @return a future implementation (CompletableFuture) once the transaction is complete, yielding the transaction result
+     */
+    <R> CompletableFuture<R> transaction(TransactionActor<R> actor);
+
+
+}
